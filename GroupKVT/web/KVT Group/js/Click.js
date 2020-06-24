966336eae8f028e061e34c55a954dc1883ecffa4
webdesignforeveryone <nguyenvukhang01@gmail.com>
2020-06-24 21:00:02 +0700

    Upload project web cho team KVT
    ae trong nhóm nhớ commit nghe

---
+++ GroupKVT/web/KVT Group/js/Click.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function($){
+$(".fa fa-facebook)".on('click', function() {
+      window.location.href = "https://www.facebook.com/profile.php?id=100007033081682";
+});
+   
+})(jQuery);
